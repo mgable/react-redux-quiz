0607995a410a56ad9c422f6bcfae3eabcf9054c8
Mark Gable <mgable43@gmail.com>
2018-03-28 14:36:31 -0700

    added game over

--- src/Quiz/components/Layout.jsx
+++ src/Quiz/components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ChoiceList from './ChoiceList.jsx';
 import './Layout.css'
+import finished from '../../billpaxtongameoveraliens.jpg'
 import Results from './Results.jsx';
 import { STATES } from '../../actions';
  
@@ -24,8 +25,7 @@ const Layout = ({image, response, total, status, correct, current, wrong, ...res
 	} else if (status === STATES.FINISH){
 		return (
 			<div className="quiz-holder">
-				<img className="wait" alt="wait spinner" src="./ajax-loader.gif" />
-				<div className="image-holder" style={itemStyle}></div>
+				<div className="image-holder"><img className="game-over" alt="game over" src={finished} /></div>
 				<div>Game Over Man!</div>
 				<Results total={total} correct={correct} wrong={wrong} current={current}></Results>
 			</div>
